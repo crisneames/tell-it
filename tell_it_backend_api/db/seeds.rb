d20452b58c05ac1bdd6c3b2edd505d683452c512
Cristi Neames <crisneames.gmail.com>
2020-04-03 12:13:01 -0500

    Added models and seeded data to backend

--- tell_it_backend_api/db/seeds.rb
+++ tell_it_backend_api/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+2.times do
+  Message.create(
+    title: Faker::Coffee.blend_name,
+    body: Faker::Quote.matz
+  )
+end
+
+# 10.times do
+#   User.create(
+#     user_name: Faker::Twitter.screen_name,
+#     user_password: Faker::Crypto.md5
+#   )
+# end
